0211d654653abeeac9cc859cb42ee0ae806a19c4
畠中晃弘 <hatanaka@questetra.com>
2022-09-01 21:45:44 +0900

    add one test

--- src/test/java/com/questetra/qlang/AddNodeTest.java
+++ src/test/java/com/questetra/qlang/AddNodeTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 public class AddNodeTest {
     @Test public void testExit() {
         try (Context context = Context.create()) {
-            Assert.assertEquals("46", context.eval("mathlang", "12+34"));
+            Assert.assertEquals(46, context.eval("mathlang", "12+34").asInt());
         } catch (PolyglotException pe) {
             Assert.assertTrue(pe.isExit());
             Assert.assertEquals(5, pe.getExitStatus());
